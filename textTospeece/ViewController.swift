9263811f26340147330843b0c236ec3dda410c2b
billal6185 <billalhossain6185@gmail.com>
2020-03-14 17:18:09 +0600

    done my project

--- textTospeece/ViewController.swift
+++ textTospeece/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+let saynthesizer = AVSpeechSynthesizer()
+    let tex = "Hello how are you"
+    
+    @IBOutlet weak var textLB: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textLB.text = tex
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+        let utterance = AVSpeechUtterance(string: tex)
+        saynthesizer.speak(utterance)
+        
+    }
+    
 }
 
